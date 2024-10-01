a49ed9167adcf930fefd2ad4134bff9af053ef4f
Warcko <bouard603@gmail.com>
2024-10-01 21:33:24 +0200

    feat : add tailwind and style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+  <div class="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
     <MovieList />
   </div>
 </template>
 
 <script>
-import MovieList from './components/MovieList.vue'; // Assurez-vous que le chemin est correct
+import MovieList from './components/MovieList.vue';
 
 export default {
   components: {
